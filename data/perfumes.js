6dd5aec0f71061734f7dc613b33f5169d14a9b49
Mengersito <98970347+Mengersito@users.noreply.github.com>
2026-01-09 14:40:16 -0500

    Create perfumes.js

---
+++ data/perfumes.js
@@ -0,0 +1,14 @@
+const perfumes = [
+  {
+    id: 1,
+    nombre: "Dior Sauvage",
+    precio: "350.000 COP",
+    imagen: "images/dior-sauvage.jpg"
+  },
+  {
+    id: 2,
+    nombre: "Bleu de Chanel",
+    precio: "420.000 COP",
+    imagen: "images/bleu-chanel.jpg"
+  }
+];
